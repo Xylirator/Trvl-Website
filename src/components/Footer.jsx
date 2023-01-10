b6c5cf07002fe03ba75cc19c7314f0a442807864
umar khan <umar.khan@student.uts.edu.au>
2023-01-10 22:10:56 +1100

    finished initial version of website

---
+++ src/components/Footer.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import "./Footer.css";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="footer__container">
+      <section className="footer__subscription">
+        <p className="footer__subscription--heading">
+          Join the Adventure newsletter to receive our best vacation deals
+        </p>
+        <p className="footer__subscription--text">
+          You can unsubscribe at any time.
+        </p>
+        <div className="input__areas">
+          <form>
+            <input
+              className="footer__input"
+              name="email"
+              type="email"
+              placeholder="Your Email"
+            />
+            <Button buttonStyle="btn__outline">Subscribe</Button>
+          </form>
+        </div>
+      </section>
+      <div class="footer__links">
+        <div className="footer__link--wrapper">
+          <div class="footer__link--items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+          <div class="footer__link--items">
+            <h2>Contact Us</h2>
+            <Link to="/">Contact</Link>
+            <Link to="/">Support</Link>
+            <Link to="/">Destinations</Link>
+            <Link to="/">Sponsorships</Link>
+          </div>
+        </div>
+        <div className="footer__link--wrapper">
+          <div class="footer__link--items">
+            <h2>Videos</h2>
+            <Link to="/">Submit Video</Link>
+            <Link to="/">Ambassadors</Link>
+            <Link to="/">Agency</Link>
+            <Link to="/">Influencer</Link>
+          </div>
+          <div class="footer__link--items">
+            <h2>Social Media</h2>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Facebook</Link>
+            <Link to="/">Youtube</Link>
+            <Link to="/">Twitter</Link>
+          </div>
+        </div>
+      </div>
+      <section class="social__media">
+        <div class="social__media--wrap">
+          <div class="footer__logo">
+            <Link to="/" className="social__logo">
+              TRVL
+              <i class="fab fa-typo3" />
+            </Link>
+          </div>
+          <small class="website__rights">TRVL © 2020</small>
+          <div class="social__icons">
+            <Link
+              class="social__icon--link facebook"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <i class="fab fa-facebook-f" />
+            </Link>
+            <Link
+              class="social__icon--link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <i class="fab fa-instagram" />
+            </Link>
+            <Link
+              class="social__icon--link youtube"
+              to="/"
+              target="_blank"
+              aria-label="Youtube"
+            >
+              <i class="fab fa-youtube" />
+            </Link>
+            <Link
+              class="social__icon--link twitter"
+              to="/"
+              target="_blank"
+              aria-label="Twitter"
+            >
+              <i class="fab fa-twitter" />
+            </Link>
+            <Link
+              class="social__icon--link twitter"
+              to="/"
+              target="_blank"
+              aria-label="LinkedIn"
+            >
+              <i class="fab fa-linkedin" />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
